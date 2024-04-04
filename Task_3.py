c6d037470622678ccb7bbb70ac8fb9b8fb478f7c
abbs4xs <165981239+abbs4xs@users.noreply.github.com>
2024-04-04 10:35:22 +0400

    Update Task_3.py
    Qarabağ Azərbaycandır!

--- Task_3.py
+++ Task_3.py
@@ -1,10 +1,25 @@
-#import libraries here
-
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    inp = (int)(input("Enter the wavelength in nm: "))
+    ans = None
+    if inp <380:
+        ans = None
+    elif inp <450:
+        ans = 'Violet'
+    elif inp <495:
+        ans = 'Blue'
+    elif inp < 570:
+        ans = 'Green'
+    elif inp < 590:
+        ans = 'Yellow'
+    elif inp < 620:
+        ans = 'Orange'
+    elif inp <= 750:
+        ans = 'Red'
+    if not ans:
+        print("Invalid input!")
+    else:
+        print(f'The relevant color is {ans}')
+    pass
 
 if __name__ == "__main__":
   main()
