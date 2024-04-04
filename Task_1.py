803f148a26bf9afe279529c1696f92e895bfd7ab
abbs4xs <165981239+abbs4xs@users.noreply.github.com>
2024-04-04 10:34:27 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,10 +1,13 @@
-#import libraries here
-
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    inp = input("Enter a letter of the alphabet: ")
+    saitler = ['a','e','i','o','u']
+    if inp in saitler:
+        print("Entered alphabet is a vowel!")
+    elif inp != 'y':
+        print("Entered alphabet is a consonant!")
+    else:
+        print("Sometimes it is a vowel, and sometimes it is a consonant!")
+    pass
 
 if __name__ == "__main__":
   main()
