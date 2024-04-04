447774bcdf342b1c77cf6f31a553bfc94f1343aa
abbs4xs <165981239+abbs4xs@users.noreply.github.com>
2024-04-04 10:35:59 +0400

    Update Task_4.py
    Doğum evi

--- Task_4.py
+++ Task_4.py
@@ -1,10 +1,23 @@
-#import libraries here
-
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    data = [
+    "Dragon",
+    "Snake",
+    "Horse",
+    "Sheep",
+    "Monkey",
+    "Rooster",
+    "Dog",
+    "Pig",
+    "Rat",
+    "Ox",
+    "Tiger",
+    "Hare"
+    ]
+    inp = (int)(input("Enter the year [ex. 2021]: "))
+    if inp < 0:
+        print("Invalid year!")
+    else:
+        print(f'{inp} is the year of the {data[(inp%12+4)%12]}')
 
 if __name__ == "__main__":
   main()
