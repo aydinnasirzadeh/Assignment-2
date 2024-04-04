657ef30b0893504a8c4fc1bf6b8a23e3e73921a4
abbs4xs <165981239+abbs4xs@users.noreply.github.com>
2024-04-04 10:37:11 +0400

    Update Task_7.py
    Oğlanlığdan bi-xəbərlər

--- Task_7.py
+++ Task_7.py
@@ -1,10 +1,12 @@
-#import libraries here
-
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    x = (float)(input("Enter x: "))
+    y = (float)(input("Enter y: "))
+    if (x>=0 and x>abs(y) and y>=0 and y>=(x-2)**2-3) or\
+        (x<=abs(y) and y<=0 and x>=0 and y>=(x-2)**2-3):
+        print("The point is in the shaded area")
+    else:
+        print("The point is not in the shaded area")
+    pass
 
 if __name__ == "__main__":
   main()
