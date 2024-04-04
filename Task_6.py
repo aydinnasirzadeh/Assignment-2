66dc06aa212c0b174bdecdc6b752f7c5ffaf5a61
abbs4xs <165981239+abbs4xs@users.noreply.github.com>
2024-04-04 10:36:42 +0400

    Update Task_6.py
    Rusiyanın vorları, Azərbaycan oğulları

--- Task_6.py
+++ Task_6.py
@@ -1,10 +1,11 @@
-#import libraries here
-
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    x = (float)(input("Enter x: "))
+    y = (float)(input("Enter y: "))
+    if (x <= 2 and y <= x and y>=0) and x >= (4-y**2)**0.5:
+        print("The point is in the shaded area")
+    else:
+        print("The point is not in the shaded area")
+    pass
 
 if __name__ == "__main__":
   main()
